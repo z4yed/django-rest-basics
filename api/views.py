066c2eef97808ae8d826f162f16a134b835eb170
Zayed <zayed.cs3@gmail.com>
2020-12-24 12:09:31 +0600

    Todo CRUD completed.

--- api/views.py
+++ api/views.py
@@ -11,10 +11,10 @@ from api.serializers import TodoSerializer
 def api_overview(request):
     urls_list = {
         'list': '/task-list/',
-        'details_view': '/task/<str:pk>/',
-        'create': '/task/create/',
-        'update': '/task/update/<str:pk>',
-        'delete': '/task/delete/<str:pk>/',
+        'details_view': '/task-detail/<task_id>/',
+        'create': '/task-create',
+        'update': '/task-update/<task_id>',
+        'delete': '/task-delete/<task_id>/',
     }
     return Response(urls_list)
 
@@ -24,3 +24,34 @@ def todo_list(request):
     todos = Todo.objects.all()
     serializers = TodoSerializer(todos, many=True)
     return Response(serializers.data)
+
+
+@api_view(['GET'])
+def todo_details(request, task_id):
+    todo = Todo.objects.get(pk=task_id)
+    serializers = TodoSerializer(todo, many=False)
+    return Response(serializers.data)
+
+
+@api_view(['POST'])
+def create_todo(request):
+    serializers = TodoSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+    return Response(serializers.data)
+
+
+@api_view(['POST'])
+def update_todo(request, task_id):
+    todo = Todo.objects.get(pk=task_id)
+    serializers = TodoSerializer(instance=todo, data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+    return Response(serializers.data)
+
+
+@api_view(['DELETE'])
+def delete_todo(request, task_id):
+    todo = Todo.objects.get(pk=task_id)
+    todo.delete()
+    return Response("Item Deleted Successfully. ")
