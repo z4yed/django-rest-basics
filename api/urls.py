76d120db6687bdbb959d7fda631f37bbbd2d790d
Zayed <zayed.cs3@gmail.com>
2020-12-24 11:24:27 +0600

    First Api View Created that displays list of urls.

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path(''),
+    path('', views.api_overview, name='overview'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def api_overview(request):
+    urls_list = {
+        'list': '/task-list/',
+        'details_view': '/task/<str:pk>/',
+        'create': '/task/create/',
+        'update': '/task/update/<str:pk>',
+        'delete': '/task/delete/<str:pk>/',
+    }
+    return Response(urls_list)
